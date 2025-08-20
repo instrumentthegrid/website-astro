a2e61d6d7c07b32e1c6ab1a4043ad9e01310e906
Philip Rosenberg-Watt <watt@gridvisibility.com>
2025-08-20 11:47:14 -0600

    YouTube embed workaround

--- src/layouts/shortcodes/Youtube.tsx
+++ src/layouts/shortcodes/Youtube.tsx
@@ -1,6 +1,16 @@
+import type { only } from "node:test";
 import React from "react";
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
+// import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
+let LiteYouTubeEmbed: any;
+
+if (typeof window !== "undefined") {
+  // Only require in browser
+  LiteYouTubeEmbed = require("react-lite-youtube-embed");
+}
+
+
 
 const Youtube = ({
   id,
@@ -11,6 +21,8 @@ const Youtube = ({
   title: string;
   [key: string]: any;
 }) => {
+  if (!LiteYouTubeEmbed) return null;
+
   return (
     <LiteYouTubeEmbed
       wrapperClass="yt-lite rounded-lg"
